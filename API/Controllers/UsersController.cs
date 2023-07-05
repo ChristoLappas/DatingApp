d2e8d5aded4504b9ccfb6cb164636ebae9fb1469
ChristoLappas <93314369+ChristoLappas@users.noreply.github.com>
2023-07-05 10:46:14 +0200

    updating resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -35,6 +36,21 @@ namespace API.Controllers
         {
             return await _repo.GetMemberAsync(username);
             
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _repo.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if(await _repo.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user!");
         }
 
     }
